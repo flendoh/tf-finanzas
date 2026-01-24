85cfd83cdd6d3aa77c0e48f472240ab0bdd7e8d0
mhallasi <flenditomlgcreations@gmail.com>
2026-01-23 22:04:55 -0500

    refactor: remove unnecessary 'solse_pe_vat' module dependency

--- addons/conexiomarket_orders/__manifest__.py
+++ addons/conexiomarket_orders/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "mhallasi",
     "category": "Integration",
     'version': '18.0.1.0.0',
-    "depends": ["conexiomarket_core", "sale", 'account', 'solse_pe_vat'],
+    "depends": ["conexiomarket_core", "sale", 'account'],
     "data": [
         "views/sale_order_views.xml",
         "views/market_account_views.xml"
